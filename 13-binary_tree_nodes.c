a937564eb2404f2508c3e48bb115289c1b118926
kipsamoh <133492659+kipsamoh@users.noreply.github.com>
2023-12-01 20:37:03 +0300

    13-binary_tree_nodes.c

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -1,27 +1,21 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_is_parent - checks if a node is a parent
+ * binary_tree_is_parent - _checks if a _node is a _parent
  *
- * @node: pointer to the node to check
- * Return: 1 if node is a parent, otherwise 0
+ * @node: pointer to the _node to check
+ * Return: 1 if node is a _parent, otherwise 0
  */
 int binary_tree_is_parent(const binary_tree_t *node)
 {
-	int parent = 0;
+	int _parent = 0;
 
 	if (node && (node->left || node->right))
-		parent = 1;
+		_parent = 1;
 
-	return (parent);
+	return (_parent);
 }
 
-/**
- * binary_tree_nodes - counts the nodes with at least 1 child in a binary tree
- *
- * @tree: tree root
- * Return: number of parent nodes in a the tree or 0 if tree is NULL;
- */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
 	if (binary_tree_is_parent(tree))
