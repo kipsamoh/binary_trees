1743bb790e95044f75d7d6dddda67cdd0f47a5ee
kipsamoh <133492659+kipsamoh@users.noreply.github.com>
2023-12-01 19:50:48 +0300

    binary_trees.h

--- binary_trees.h
+++ binary_trees.h
@@ -7,10 +7,10 @@
 /**
  * struct binary_tree_s - Binary tree node
  *
- * @n: Integer stored in the node
- * @parent: Pointer to the parent node
- * @left: Pointer to the left child node
- * @right: Pointer to the right child node
+ * @n: Integer stored in the _node
+ * @parent: Pointer to the _parent node
+ * @left: Pointer to the _left child node
+ * @right: Pointer to the _right child node
  */
 struct binary_tree_s
 {
