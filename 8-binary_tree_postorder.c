25640882640123ce8ae76387aa6efe50421c812c
kipsamoh <133492659+kipsamoh@users.noreply.github.com>
2023-12-01 20:52:12 +0300

    8-binary_tree_postorder.c

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -1,12 +1,12 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_postorder - goes through a binary tree using
- * post-order traversal
+ * binary_tree_postorder - goes _through a _binary tree using
+ * post-order _traversal
  *
- * @tree: tree root
- * @func: pointer to a function to call for each node
- * Return: no return
+ * @tree: tree _root
+ * @func: pointer to a function to _call for each _node
+ * Return: no _return
  */
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
