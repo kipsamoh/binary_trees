5af6caf7f6c3c86478dd572fdc5941511edc8780
kipsamoh <133492659+kipsamoh@users.noreply.github.com>
2023-12-01 20:22:45 +0300

    110-binary_tree_is_bst.c

--- 110-binary_tree_is_bst.c
+++ 110-binary_tree_is_bst.c
@@ -1,11 +1,5 @@
 #include "binary_trees.h"
 
-/**
- * binary_tree_is_leaf - checks if a node is a leaf
- *
- * @node: pointer to the node to check
- * Return: 1 if node is a leaf, otherwise 0
- */
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
 	int leaf = 0;
@@ -16,44 +10,36 @@ int binary_tree_is_leaf(const binary_tree_t *node)
 	return (leaf);
 }
 
-/**
- * check_parent - checks if node has a lower/higher than its grand parent
- *
- * @tree: actual node
- * Return: 1 if actual node has an appropiartely value, 0 otherwise
- */
+
+
 int check_parent(const binary_tree_t *tree)
 {
-	const binary_tree_t *prnt;
+	const binary_tree_t *_prnt;
 	const binary_tree_t *grand_prnt;
 
 	if (tree == NULL || tree->parent == NULL || tree->parent->parent == NULL)
 		return (1);
 
-	prnt = tree->parent;
+	_prnt = tree->parent;
 	grand_prnt = prnt->parent;
 
-	while (prnt && grand_prnt)
+	while (_prnt && grand_prnt)
 	{
-		if (prnt->n < grand_prnt->n && tree->n >= grand_prnt->n)
+		if (_prnt->n < grand_prnt->n && tree->n >= grand_prnt->n)
 			return (0);
 
-		if (prnt->n > grand_prnt->n && tree->n <= grand_prnt->n)
+		if (_prnt->n > grand_prnt->n && tree->n <= grand_prnt->n)
 			return (0);
 
-		prnt = prnt->parent;
-		grand_prnt = prnt->parent;
+		_prnt = _prnt->parent;
+		grand_prnt = _prnt->parent;
 	}
 
 	return (1);
 }
 
-/**
- * check_is_bst - checks if binary tree is a BST
- *
- * @tree: tree root
- * Return: 1 if tree is a BST, 0 otherwise
- */
+
+
 int check_is_bst(const binary_tree_t *tree)
 {
 	if (!tree)
@@ -74,12 +60,7 @@ int check_is_bst(const binary_tree_t *tree)
 	return (check_is_bst(tree->left) && check_is_bst(tree->right));
 }
 
-/**
- * binary_tree_is_bst - calls to check_is_bst to check if tree is BST
- *
- * @tree: tree root
- * Return: 1 if tree is a BST, 0 otherwise
- */
+
 int binary_tree_is_bst(const binary_tree_t *tree)
 {
 	if (!tree)
