bf814a1daa74ca83104627b26eb1961508a1c8fd
kipsamoh <133492659+kipsamoh@users.noreply.github.com>
2023-12-01 20:25:24 +0300

    113-bst_search.c

--- 113-bst_search.c
+++ 113-bst_search.c
@@ -1,12 +1,6 @@
 #include "binary_trees.h"
 
-/**
- * bst_search - searches for a value in a Binary Search Tree
- *
- * @tree: tree root
- * @value: node value
- * Return: pointer the found node
- */
+
 bst_t *bst_search(const bst_t *tree, int value)
 {
 	if (tree && value < tree->n)
